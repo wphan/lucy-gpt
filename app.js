ccb5f502e325f47a4f13d28577289f7d844c5127
wphan <william@drift.trade>
2023-03-15 21:36:21 -0700

    start app for socket

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.action('button_click', async ({ body, ack, say }) => {
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start();
 
   console.log('⚡️ Bolt app is running!');
 })();
