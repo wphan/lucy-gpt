c23d2405e1cec85a919a40979590bea022f91aef
wphan <william@drift.trade>
2023-03-15 21:18:58 -0700

    try fix auth

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ see: https://slack.dev/bolt-js/tutorial/getting-started
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   socketMode: false,
-  appToken: process.env.SLACK_APP_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
